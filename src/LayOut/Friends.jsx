e5b2d060f48f2f2af4b70eee0d483aeb1f0ae666
anuj paul <paulanuj27@gmail.com>
2026-01-01 20:07:05 +0600

    ** Dynamic Follow Button Feature **

--- src/LayOut/Friends.jsx
+++ src/LayOut/Friends.jsx
@@ -3,41 +3,53 @@ import axios from "axios";
 
 const Friends = () => {
   const [allFriend, setFriend] = useState([]);
+  const [active, setActive] = useState([]);
   useEffect(() => {
-      axios.get(`http://localhost:5000/users`).then((res) => setFriend(res.data));
-    }, []);
-    
+    axios.get(`http://localhost:5000/users`).then((res) => setFriend(res.data));
+  }, []);
 
+  useEffect(()=>{
+    setActive(Array(allFriend.length).fill(false))
+  },[allFriend])
+
+  function handleFollowBtn (i)
+  {
+    const copy = [...active]
+    copy[i] = !copy[i]
+    setActive(copy)
+  }
 
   return (
     <>
-      <p> {allFriend.length} </p>
       {allFriend.map((user, index) => {
         return (
-          <div className="w-3/4 mx-auto bg-gray-800 py-2 px-2 rounded-lg mt-4">
+          <div key={index} className="w-3/4 mx-auto bg-gray-800 py-2 px-2 rounded-lg mt-4">
             {/* User */}
-            <div class="flex items-center space-x-4  ">
-              <div class="flex-shrink-0">
+            <div className="flex items-center space-x-4  ">
+              <div className="flex-shrink-0">
                 <img
                   className="w-20 rounded-full"
                   src={user?.photoURL}
                   alt=""
                 />
               </div>
-              <div class="flex-1 min-w-0">
-                <p class="text-xl font-medium light:text-gray-900 truncate dark:text-white">
+              <div className="flex-1 min-w-0">
+                <p className="text-xl font-medium light:text-gray-900 truncate dark:text-white">
                   {user?.name}
                 </p>
-                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                   From:
                 </p>
-                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                   Work at:
                 </p>
               </div>
-              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                <button>
-                    
+              <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                <button
+                onClick={()=> handleFollowBtn(index)}
+                className={`btn ${active[index]? 'btn-secondary': 'btn-outline'}`}
+                >
+                    {active[index]? 'follow':'unfollow'}
                 </button>
               </div>
             </div>
