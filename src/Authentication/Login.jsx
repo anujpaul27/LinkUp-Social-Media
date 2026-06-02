a766d22ab5764dad2529299e4b2cad9a0eb97dfd
anuj paul <paulanuj27@gmail.com>
2026-06-02 11:41:52 +0600

    update login button with loading animation

--- src/Authentication/Login.jsx
+++ src/Authentication/Login.jsx
@@ -3,24 +3,28 @@ import { Link, useNavigate } from "react-router";
 import { UserContext } from "../Context/ContextProvider";
 
 function Login() {
-  const [error,setError] = useState("")
-  const navigation = useNavigate()
-  const {login} = useContext(UserContext) 
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigate();
+  const { login } = useContext(UserContext);
 
   function handleSubmit(event) {
     event.preventDefault();
+    setError("")
     const form = event.target;
-    const formValue = new FormData(form)
-    const Obj = Object.fromEntries(formValue.entries())
+    const formValue = new FormData(form);
+    const Obj = Object.fromEntries(formValue.entries());
+    setLoading(true);
 
     login(Obj.email, Obj.password)
-    .then(res=> {
-      navigation('/')
-    })
-    .catch(error=> {
-      setError(error.message);
-    })
-
+      .then((res) => {
+        navigation("/");
+      })
+      .catch((error) => {
+        console.error(error.message);
+        setError("Invalid User!. Please enter valid email and password.");
+        setLoading(false);
+      });
   }
 
   return (
@@ -59,22 +63,46 @@ function Login() {
 
           {/* Error Message */}
           {error && (
-            <p className="text-red-500 text-sm font-semibold">{error}</p>
+            <p className="text-red-500 text-sm flex justify-center ">{error}</p>
           )}
 
           {/* Forget password */}
           <div className="flex justify-between items-center">
-            <button className="btn btn-link">Forget Password</button>
-            <Link className="btn btn-link" to={'/registration'}>Registration</Link>
+            <button className="btn btn-link text-white ">Forget Password</button>
+            <Link className="btn btn-link text-white " to={"/registration"}>
+              Registration
+            </Link>
           </div>
 
           {/* Buttons */}
           <div className="flex w-full gap-4 pt-4">
             <button
               type="submit"
-              className="btn btn-primary flex-1 rounded-full shadow-lg hover:shadow-xl hover:scale-105 transition-all"
+              className={`btn ${!loading && 'btn-primary'} flex-1 rounded-full shadow-lg hover:shadow-xl hover:scale-105 transition-all `}
             >
               Submit
+              {loading && 
+                <svg
+                className="animate-spin h-5 w-5 text-white"
+                xmlns="http://w3.org"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              }
             </button>
             <button
               type="reset"
